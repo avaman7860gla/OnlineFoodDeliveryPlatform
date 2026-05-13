0b017cc4b848ba8d459595913523ce0cf09e14ba
Aman <avaman7860@gmail.com>
2026-05-13 22:16:47 +0530

    [Aman Verma] Add. databases of all services(MySQL)

---
+++ QuickBite-Database.sql
@@ -0,0 +1,28 @@
+create database auth_db;
+use auth_db;
+
+create database restaurant_db;
+use restaurant_db;
+
+create database menu_db;
+use menu_db;
+
+create database cart_db;
+use cart_db;
+
+create database order_db;
+use order_db;
+
+create database payment_db;
+use payment_db;
+
+
+create database delivery_db;
+use delivery_db;
+
+
+create database notification_db;
+use notification_db;
+
+show tables;
+
